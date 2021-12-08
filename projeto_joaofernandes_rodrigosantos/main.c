d2143fb66f057f26820b5759536883743be52573
Rodrigo Santos <rodrigo.s.santos003@gmail.com>
2021-12-08 13:36:50 +0000

    Added function for menu

--- projeto_joaofernandes_rodrigosantos/main.c
+++ projeto_joaofernandes_rodrigosantos/main.c
@@ -8,36 +8,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void menu(void);
+
 int main()
 {
-    //declaração de variáveis
-    int opcaoMenu;
-
     printf("BANCO ALVES DOS REIS\n");
 
-    //menu do programa
-    do{
-        printf("1. Abrir uma conta\n");
-        printf("2. Listar contas existentes\n");
-        printf("3. Consultar detalhes de uma conta\n");
-        printf("4. Depositar dinheiro numa conta\n");
-        printf("5. Levantar dinheiro de uma conta\n");
-        printf("6. Transferir dinheiro entre contas\n");
-        printf("7. Editar a informacao de uma conta\n");
-        printf("8. Eliminar uma conta\n");
-        printf("9. Gravar listagem de contas para um ficheiro de texto\n\n");
-        printf("10. Sair do programa\n");
-
-        printf("Escolha uma opcao do menu: ");
-        scanf("%d", &opcaoMenu);
-
-        /*
-        switch(opcaoMenu){
-
-        }
-        */
-
-    }while(opcaoMenu != 10);
+    menu();
 
     return 0;
+}
+
+void menu() {
+    int opcaoMenu;
+
+    printf("1. Abrir uma conta\n");
+    printf("2. Listar contas existentes\n");
+    printf("3. Consultar detalhes de uma conta\n");
+    printf("4. Depositar dinheiro numa conta\n");
+    printf("5. Levantar dinheiro de uma conta\n");
+    printf("6. Transferir dinheiro entre contas\n");
+    printf("7. Editar a informacao de uma conta\n");
+    printf("8. Eliminar uma conta\n");
+    printf("9. Gravar listagem de contas para um ficheiro de texto\n\n");
+    printf("10. Sair do programa\n");
+
+    printf("Escolha uma opcao do menu: ");
+    scanf("%d", &opcaoMenu);
+
+    switch(opcaoMenu){
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        case 10:
+            exit(0);
+            break;
+    }
 }
